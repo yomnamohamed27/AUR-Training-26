fabebdf59ac7f2ef6d221b35fda495f7f6e7a821
yomnamohamed <yoomnamohamedd226@gmail.com>
2026-08-24 22:02:20 +0300

    Remove subtask_2

--- subtask_2/src/subtask_2/__init__.py
+++
@@ -1,2 +0,0 @@
-def main() -> None:
-    print("Hello from subtask-2!")
--- subtask_2/src/subtask_2/__main__.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image 
-def main():   
-    image = Image.open("img.png")
-    bw_image = image.convert("L")  
-    bw_image.show()
-main()
